bf26c8aeac4b8f7da6def6874923159ba2cdd9bf
shivaprasadkyathanatty <shivaprasad.kyathanatty.contractor@macmillan.com>
2025-03-01 14:16:08 +0530

    modified to accomodate github action

--- google_load_test_selenium.py
+++ google_load_test_selenium.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 def test_google_loads():
-    driver = webdriver.Chrome()  # Use Firefox() if needed
-    driver.get("https://www.google.com")  # Open Google
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service)
+    driver.get("https://www.google.com")
 
     assert "Google" in driver.title  # Check if "Google" is in the page title
     driver.quit()
---
+++ test_script.py
@@ -0,0 +1,5 @@
+def test_addition():
+	assert 2+2==4
+if __name__=="__main__":
+	test_addition()
+	print("All Tests Passed")
